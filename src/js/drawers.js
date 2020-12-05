42e76f34bfc9a1e8a5a725e7c84ae3316c2e310d
Yegor Bitensky <yegorbitensky@gmail.com>
2020-12-06 01:55:53 +0500

    Add days drawers class and days line

--- src/js/drawers.js
+++ src/js/drawers.js
@@ -4,12 +4,10 @@ class BaseDrawer {
   /**
    * Create.
    * @param {string} canvasId - Canvas id.
-   * @param {integer} maxValue - Maximal value.
    */
-  constructor(canvasId, maxValue) {
+  constructor(canvasId) {
     this._canvas = document.getElementById(canvasId);
     this._ctx = this._canvas.getContext('2d');
-    this._maxValue = maxValue;
   }
 
   /** Clear canvas. */
@@ -34,6 +32,15 @@ class BaseDrawer {
   _getDeltaValue(date) {}
 
   /**
+   * Get value.
+   * @abstract
+   * @param {integer} currentValue - Current value.
+   * @param {integer} index - Value index.
+   * @return {integer|string} Value.
+   */
+  _getValue(currentValue, index) {}
+
+  /**
    * Draw values line.
    * @param {Date} date - Date instance.
    */
@@ -43,10 +50,7 @@ class BaseDrawer {
     let delta = this._getDeltaValue(date);
     for (let i = 0; i < 11; i++) {
       let x = 20 * i - delta;
-      let value = currentValue + i;
-      if (value > this._maxValue - 1) {
-        value -= this._maxValue;
-      }
+      let value = this._getValue(currentValue, i);
       if (value < 10) {
         value = `0${value}`;
       }
@@ -56,8 +60,77 @@ class BaseDrawer {
 }
 
 
+/** Base solid numbers draw logic. */
+class BaseSolidNumbersDrawer extends BaseDrawer {
+
+  /**
+   * Create.
+   * @param {string} canvasId - Canvas id.
+   * @param {integer} maxValue - Maximal value.
+   */
+  constructor(canvasId, maxValue) {
+    super(canvasId);
+    this._maxValue = maxValue;
+  }
+
+  /**
+   * Get value.
+   * @param {integer} currentValue - Current value.
+   * @param {integer} index - Value index.
+   * @return {integer|string} Value.
+   */
+  _getValue(currentValue, index) {
+    let value = currentValue + index;
+    if (value > this._maxValue - 1) {
+      value -= this._maxValue;
+    }
+    return value;
+  }
+}
+
+
+/** Days line draw class. */
+class DaysDrawer extends BaseDrawer {
+
+  /**
+   * Create.
+   * @param {string} canvasId - Canvas id.
+   */
+  constructor(canvasId) {
+    super(canvasId);
+  }
+
+  /**
+   * Get current unix time value.
+   * @param {Date} date - Date instance.
+   * @return {integer} Unix time.
+   */
+  _getCurrentValue(date) {
+    return date.getTime();
+  }
+
+  /**
+   * Get day delta value.
+   * @param {Date} date - Date instance.
+   * @return {integer} Delta value.
+   */
+  _getDeltaValue(date) {
+    return Math.round(date.getHours() / 1.2);
+  }
+
+  /**
+   * Get day value.
+   * @param {integer} unixValue - Unix time.
+   * @param {integer} daysLeft - Days left since unixValue.
+   */
+  _getValue(unixValue, daysLeft) {
+    return (new Date(unixValue + 86400000 * daysLeft)).getDate();
+  }
+}
+
+
 /** Hours line draw class. */
-class HoursDrawer extends BaseDrawer {
+class HoursDrawer extends BaseSolidNumbersDrawer {
 
   /**
    * Create.
@@ -88,7 +161,7 @@ class HoursDrawer extends BaseDrawer {
 
 
 /** Minutes line draw class. */
-class MinutesDrawer extends BaseDrawer {
+class MinutesDrawer extends BaseSolidNumbersDrawer {
 
   /**
    * Create.
@@ -119,7 +192,7 @@ class MinutesDrawer extends BaseDrawer {
 
 
 /** Seconds line draw class. */
-class SecondsDrawer extends BaseDrawer {
+class SecondsDrawer extends BaseSolidNumbersDrawer {
 
   /**
    * Create.
--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', (e) => {
+  let days = new DaysDrawer('days-line');
   let hours = new HoursDrawer('hours-line');
   let minutes = new MinutesDrawer('minutes-line');
   let seconds = new SecondsDrawer('seconds-line');
 
   setInterval(() => {
     let date = new Date();
+    days.draw(date);
     hours.draw(date);
     minutes.draw(date);
   }, 1000);
